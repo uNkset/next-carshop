01726aa5566376629e11aa9349121892c26533d1
Konstantin Arescenko <kostylo@windowslive.com>
2023-08-17 14:16:39 +0200

    basic hero section look done

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,37 @@
+'use client'
+
 import Image from 'next/image'
+import CustomButton from './CustomButton'
 
 const Hero = () => {
-  return <div>Hero</div>
+  const handleScroll = () => {}
+
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          Your best way to find, book, or rent a car — safe your time and
+          energy!
+        </h1>
+        <p className="hero__subtitle">
+          Streamline your car rental experience with our effortless booking
+          process.
+        </p>
+
+        <CustomButton
+          title="Explore Cars"
+          customStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" objectFit="contain" fill />
+          <div className="hero__image-overlay" />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Hero
