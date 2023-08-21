e39d70553d2bc6b6b5e2abe5fa7065cc68486736
Konstantin Arescenko <kostylo@windowslive.com>
2023-08-21 12:20:13 +0200

    basic nav and footer done

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -34,8 +34,8 @@ const Hero = () => {
             fill
             priority
           />
-          <div className="hero__image-overlay" />
         </div>
+        <div className="hero__image-overlay" />
       </div>
     </div>
   )
