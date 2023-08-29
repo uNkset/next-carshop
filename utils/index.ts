fc3fc5140f95980bf9ab7114c0f111ebc15035f6
Konstantin Arescenko <kostylo@windowslive.com>
2023-08-29 12:36:02 +0200

    testing api

--- utils/index.ts
+++ utils/index.ts
@@ -1,11 +1,11 @@
 export async function fetchCars() {
   const headers = {
-    'X-RapidAPI-Key': '742b027497msh30c40ff9b27b1f8p1a70fcjsn252a4936dbe0',
+    'X-RapidAPI-Key': `${process.env.API_KEY}`,
     'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com',
   }
 
   const response = await fetch(
-    'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars',
+    'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla',
     { headers: headers }
   )
 
